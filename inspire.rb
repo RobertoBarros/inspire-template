bc174b0b23e726668486f3a12e6c7d2c9d86a2ae
Roberto Barros <roberto.f.barros@gmail.com>
2025-10-16 11:54:23 -0300

    authenticated and unauthenticated layouts

--- inspire.rb
+++ inspire.rb
@@ -1,4 +1,19 @@
 ####################################################
+# Helpers
+
+# replace a pattern in a file while preserving the original indentation
+def gsub_file_preserving_indent(file_path, pattern, replacement_content)
+  content = File.read(file_path)
+  new_content = content.gsub(/^(\s*)#{Regexp.escape(pattern)}/) do
+    indent = $1
+    replacement_content.split("\n").map { |line| "#{indent}#{line}" }.join("\n")
+  end
+  File.write(file_path, new_content)
+end
+
+
+
+####################################################
 # Live reload with Hotwire Spark
 inject_into_file "Gemfile", after: "group :development, :test do\n" do
   <<~RUBY
@@ -122,8 +137,21 @@ run "cp #{tmp_dir}/helpers/* app/helpers/"
 run "mkdir -p app/views/devise"
 run "cp -r #{tmp_dir}/views/devise/* app/views/devise/"
 
-gsub_file "app/views/layouts/application.html.erb", "<%= yield %>", "<%= render user_signed_in? ? \"layouts/authenticated\" : \"layouts/unauthenticated\" %>"
+
+####################################################
+# Layouts
+
+# duplicate application layout to create an unauthenticated layout
+run "cp app/views/layouts/application.html.erb app/views/layouts/unauthenticated.html.erb"
+
+unauthenticated_body = File.read("#{tmp_dir}/views/layouts/unauthenticated_body.html.erb")
+gsub_file_preserving_indent "app/views/layouts/unauthenticated.html.erb", "<%= yield %>", unauthenticated_body
+
+authenticated_body = File.read("#{tmp_dir}/views/layouts/authenticated_body.html.erb")
+gsub_file_preserving_indent "app/views/layouts/application.html.erb", "<%= yield %>", authenticated_body
+
 run "cp -r #{tmp_dir}/views/layouts/_* app/views/layouts/"
+
 
 ####################################################
 # Rails Routes
@@ -144,20 +172,31 @@ run "bin/rails generate controller Pages home dashboard --skip-routes"
 inject_into_class "app/controllers/pages_controller.rb", "PagesController" do
   "  skip_before_action :authenticate_user!, only: :home\n\n"
 end
+
+inject_into_file "app/controllers/pages_controller.rb", <<-RUBY,
+
+    render layout: "unauthenticated" # Use this layout to not render sidebar
+RUBY
+  after: "def home\n"
+
 run "mkdir -p app/views/pages"
 run "cp -r #{tmp_dir}/views/pages/* app/views/pages/"
+
+
+####################################################
+# Assets copy
 run "cp -r #{tmp_dir}/assets/images/* app/assets/images/"
 
 
-########################################
-# Reset and prepare database
+####################################################
+# Cleanup before bundle
 run "bin/rails db:drop db:create db:migrate db:seed"
 
 # remove the temporary clone
 run %(rm -rf #{tmp_dir})
 
 
-########################################
+####################################################
 # After bundle tasks
 after_bundle do
   # A Stimulus controller for showing notifications.
